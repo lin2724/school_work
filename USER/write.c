552311adfd4d7683580d4b1cdb7689fff5ba1c8a
lin2724 <lin2724@msn.com>
2015-03-26 17:49:44 +0800

    write
    write 2015.3.26

---
+++ USER/write.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+unsigned int CurPiontX=0;
+unsigned int CurPiontY=0;
+
+
+void Xmoto(int dir, int step)
+{
+	
+}
+
+void Ymoto(int dir, int step)
+{
+	
+}
+
+void MovePoint(unsigned int desx,unsigned int desy)
+{
+	int lengthx,lengthy;
+
+}
+
+
+void write(char* dat)
+{
+	
+}
